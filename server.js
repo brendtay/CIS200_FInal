85e5c84ad0c521c8a720db6a3cb6ff90bf2f7bc6
Brendan T <48305105+brendtay@users.noreply.github.com>
2023-04-18 21:13:55 -0400

    changed pool

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '2014',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
     port: 5432,
 })
 
